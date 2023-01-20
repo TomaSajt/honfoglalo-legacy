2677344c22f01d7dea5d41fbcceefe61625727c7
Toma <tomasajtofficial@gmail.com>
2023-01-20 13:32:06 +0100

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapterNode()
+	}
+};

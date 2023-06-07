cc83c3d7cdc82f576afdf036b4bbed37b6d7611c
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-06-07 21:30:24 +0200

    Add nowraps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
     <main
         class="relative flex-grow p-4 flex flex-col items-center gap-2 justify-center"
     >
-        <div class="text-7xl pb-10">Honfoglaló Legacy</div>
+        <div class="text-7xl pb-10 whitespace-nowrap overflow-x-hidden">Honfoglaló Legacy</div>
         <a
             class="bg-orange-400 hover:bg-slate-200 py-3 w-40 border-2 border-black rounded-xl font-semibold text-center"
             href="/game"
@@ -37,7 +37,7 @@
     </main>
 
     <footer
-        class="grid place-items-center h-20 bg-green-500 text-white font-serif"
+        class="grid place-items-center h-20 bg-green-500 text-white font-serif whitespace-nowrap overflow-x-hidden"
     >
         <div>
             Készült Magyarország Kormánya megbízásából

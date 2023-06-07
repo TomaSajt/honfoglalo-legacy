d3132d6f02a553b84f930f3013c98919393770dc
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-06-07 12:29:54 +0200

    Add empty settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<div class="h-full grid place-items-center">
+    <div class="flex flex-col gap-4 items-center">
+        <div>Itt sajnos még nincs semmi :(</div>
+        <a href='/' class='bg-slate-200 hover:bg-slate-300 py-2 px-4 rounded'>Vissza a főmenübe</a>
+    </div>
+</div>
